salv-orlando/quantum

Update to use OSLO db
This code implements the blueprint oslo-db-support

NOTES:
1. The new section is database (this is backward compatible
   with DATABASE)
2. The sql_* are deprecated. They are also backward compatible.
3. Eventlets DB pool is no longer supported

Change-Id: I6fa7c3817f6a5d8bef48382e0e754b88521f2289

quantum/db/migration/alembic_migrations/env.py
@@ -54,7 +54,7 @@ def run_migrations_offline():
     script output.
 
     """
-    context.configure(url=quantum_config.DATABASE.sql_connection)
+    context.configure(url=quantum_config.database.sql_connection)
 
     with context.begin_transaction():
         context.run_migrations(active_plugin=quantum_config.core_plugin,
@@ -69,7 +69,7 @@ def run_migrations_online():
 
     """
     engine = create_engine(
-        quantum_config.DATABASE.sql_connection,
+        quantum_config.database.sql_connection,
         poolclass=pool.NullPool)
 
     connection = engine.connect()

openstack-archive/vmware-nsx

quantum/db/migration/alembic_migrations/env.py
@@ -54,7 +54,7 @@ def run_migrations_offline():
     script output.
 
     """
-    context.configure(url=quantum_config.DATABASE.sql_connection)
+    context.configure(url=quantum_config.database.connection)
 
     with context.begin_transaction():
         context.run_migrations(active_plugin=quantum_config.core_plugin,
@@ -69,7 +69,7 @@ def run_migrations_online():
 
     """
     engine = create_engine(
-        quantum_config.DATABASE.sql_connection,
+        quantum_config.database.connection,
         poolclass=pool.NullPool)
 
     connection = engine.connect()