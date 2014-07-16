rossella/neutron-dvr

Add L3 Extension for Distributed Routers
This patch introduces the model and extension
framework for implementing distributed virtual
routing on top of Open vSwitch.

A new admin-only 'distributed' (as opposed to a
'centralized' or legacy-mode) attribute is added
to the API router resource. It is possible to convert
an existing (centralized) router to a distributed
one; the opposite conversion, even though allowed by
the API, may not be honored by the underlying
plugin implementation and an appropriate error will
be reported.

When creating a router (regardless of the user role),
Neutron will rely on a system wide configuration, whose
default currently allows to create 'centralized' routers.

Tests are added for basic unit coverage; when the first
building blocks for neutron-testing-refactor
are complete, functional testing will be added.
This is because we should be moving away from how
extension tests have been done up until now.

Partially-implements: blueprint neutron-ovs-dvr

DocImpact

Change-Id: I7227fbe2718eba6665a5afb5dcaaaa77b341091f
Authored-by:    Swaminathan Vasudevan <swaminathan.vasudevan@hp.com>
Co-Authored-By: Armando Migliaccio <armamig@gmail.com>

neutron/db/l3_rpc_base.py
@@ -126,3 +126,56 @@ class L3RpcCallbackMixin(object):
             for fip_id in fips_to_disable:
                 l3_plugin.update_floatingip_status(
                     context, fip_id, constants.FLOATINGIP_STATUS_DOWN)
+
+    def get_ports_by_subnet(self, context, **kwargs):
+        """DVR: RPC called by dvr-agent to get all ports for subnet."""
+        subnet_id = kwargs.get('subnet_id')
+        LOG.debug("DVR: subnet_id: %s", subnet_id)
+        filters = {'fixed_ips': {'subnet_id': [subnet_id]}}
+        plugin = manager.NeutronManager.get_plugin()
+        return plugin.get_ports(context, filters=filters)
+
+    def get_agent_gateway_port(self, context, **kwargs):
+        """Get Agent Gateway port for FIP.
+
+        l3 agent expects an Agent Gateway Port to be returned
+        for this query.
+        """
+        network_id = kwargs.get('network_id')
+        host = kwargs.get('host')
+        context = neutron_context.get_admin_context()
+        plugin = manager.NeutronManager.get_plugin()
+        l3plugin = manager.NeutronManager.get_service_plugins()[
+            plugin_constants.L3_ROUTER_NAT]
+        agent_port = l3plugin.create_fip_agent_gw_port_if_not_exists(
+            context, network_id, host)
+        self._ensure_host_set_on_port(context, plugin, host,
+                                      agent_port)
+        LOG.debug('Agent Gateway port returned : %(agent_port)s with '
+                  'host %(host)s', {'agent_port': agent_port,
+                  'host': host})
+        return agent_port
+
+    def get_snat_router_interface_ports(self, context, **kwargs):
+        """Get SNAT serviced Router Port List.
+
+        The Service Node that hosts the SNAT service requires
+        the ports to service the router interfaces.
+        This function will check if any available ports, if not
+        it will create ports on the routers interfaces and
+        will send a list to the L3 agent.
+        """
+        router_id = kwargs.get('router_id')
+        host = kwargs.get('host')
+        context = neutron_context.get_admin_context()
+        plugin = manager.NeutronManager.get_plugin()
+        l3plugin = manager.NeutronManager.get_service_plugins()[
+            plugin_constants.L3_ROUTER_NAT]
+        snat_port_list = l3plugin.create_snat_intf_port_list_if_not_exists(
+            context, router_id)
+        for p in snat_port_list:
+            self._ensure_host_set_on_port(context, plugin, host, p)
+        LOG.debug('SNAT interface ports returned : %(snat_port_list)s '
+                  'and on host %(host)s', {'snat_port_list': snat_port_list,
+                  'host': host})
+        return snat_port_list

openstack-archive/vmware-nsx

neutron/db/l3_rpc_base.py
@@ -126,3 +126,56 @@ class L3RpcCallbackMixin(object):
             for fip_id in fips_to_disable:
                 l3_plugin.update_floatingip_status(
                     context, fip_id, constants.FLOATINGIP_STATUS_DOWN)
+
+    def get_ports_by_subnet(self, context, **kwargs):
+        """DVR: RPC called by dvr-agent to get all ports for subnet."""
+        subnet_id = kwargs.get('subnet_id')
+        LOG.debug("DVR: subnet_id: %s", subnet_id)
+        filters = {'fixed_ips': {'subnet_id': [subnet_id]}}
+        plugin = manager.NeutronManager.get_plugin()
+        return plugin.get_ports(context, filters=filters)
+
+    def get_agent_gateway_port(self, context, **kwargs):
+        """Get Agent Gateway port for FIP.
+
+        l3 agent expects an Agent Gateway Port to be returned
+        for this query.
+        """
+        network_id = kwargs.get('network_id')
+        host = kwargs.get('host')
+        admin_ctx = neutron_context.get_admin_context()
+        plugin = manager.NeutronManager.get_plugin()
+        l3plugin = manager.NeutronManager.get_service_plugins()[
+            plugin_constants.L3_ROUTER_NAT]
+        agent_port = l3plugin.create_fip_agent_gw_port_if_not_exists(
+            admin_ctx, network_id, host)
+        self._ensure_host_set_on_port(admin_ctx, plugin, host,
+                                      agent_port)
+        LOG.debug('Agent Gateway port returned : %(agent_port)s with '
+                  'host %(host)s', {'agent_port': agent_port,
+                  'host': host})
+        return agent_port
+
+    def get_snat_router_interface_ports(self, context, **kwargs):
+        """Get SNAT serviced Router Port List.
+
+        The Service Node that hosts the SNAT service requires
+        the ports to service the router interfaces.
+        This function will check if any available ports, if not
+        it will create ports on the routers interfaces and
+        will send a list to the L3 agent.
+        """
+        router_id = kwargs.get('router_id')
+        host = kwargs.get('host')
+        admin_ctx = neutron_context.get_admin_context()
+        plugin = manager.NeutronManager.get_plugin()
+        l3plugin = manager.NeutronManager.get_service_plugins()[
+            plugin_constants.L3_ROUTER_NAT]
+        snat_port_list = l3plugin.create_snat_intf_port_list_if_not_exists(
+            admin_ctx, router_id)
+        for p in snat_port_list:
+            self._ensure_host_set_on_port(admin_ctx, plugin, host, p)
+        LOG.debug('SNAT interface ports returned : %(snat_port_list)s '
+                  'and on host %(host)s', {'snat_port_list': snat_port_list,
+                  'host': host})
+        return snat_port_list