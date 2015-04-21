openstack-archive/vmware-nsx

commit 1b1f7a0c964f9abf74fd99bc80f98480bda574ee

Fix "Lock wait timeout" DB error on router binding
Nsxv reports "Lock wait timeout" error time to time in parallel test
cases. It happens when two subtransactions belong to different
greenthreads update/delete the same row of db. The patch is trying to
avoid the condition with these following methods:
1. Get ops outside of transaction as much as possible including db
query and _update_router_gw_info.
2. By observation, somtime server would get deleted router binding. So
create a new session everytime to get/delete_nsxv_router_binding.
3. retry on allocating edge if "Lock wait timeout" occurs
4. add "lock" on all allocating dhcp edge vnic db op to avoid edge_vnic
binding lock wait out error.
5. replace admin_context with context.elevated()

Change-Id: Ic0155f8c0cb202b4e927b681556cad382e82e455

vmware_nsx/neutron/plugins/vmware/plugins/nsx_v.py
@@ -28,7 +28,6 @@ from neutron.api.v2 import attributes as attr
 from neutron.common import constants
 from neutron.common import exceptions as n_exc
 from neutron.common import utils
-from neutron import context as neutron_context
 from neutron.db import agents_db
 from neutron.db import db_base_plugin_v2
 from neutron.db import external_net_db
@@ -984,13 +983,13 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
 
         with context.session.begin(subtransactions=True):
             super(NsxVPluginV2, self).delete_subnet(context, id)
+            if subnet['enable_dhcp']:
+                # There is only DHCP port available
+                if len(ports) == 1:
+                    port = ports.pop()
+                    self._delete_port(context, port['id'])
 
         if subnet['enable_dhcp']:
-            # There is only DHCP port available
-            if len(ports) == 1:
-                port = ports.pop()
-                self._delete_port(context, port['id'])
-
             # Delete the DHCP edge service
             network_id = subnet['network_id']
             filters = {'network_id': [network_id]}
@@ -1055,7 +1054,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
         # The DHCP for network with different physical network can not be used
         # The flat network should be located in different DHCP
         conflicting_networks = []
-        network_ids = self.get_networks(neutron_context.get_admin_context(),
+        network_ids = self.get_networks(context.elevated(),
                                         fields=['id'])
         phy_net = nsxv_db.get_network_bindings(context.session, network_id)
         if phy_net:
@@ -1186,7 +1185,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
 
     def _extract_external_gw(self, context, router, is_extract=True):
         r = router['router']
-        gw_info = None
+        gw_info = attr.ATTR_NOT_SPECIFIED
         # First extract the gateway info in case of updating
         # gateway before edge is deployed.
         if 'external_gateway_info' in r:

vmware_nsx/neutron/plugins/vmware/dbexts/nsxv_db.py
@@ -57,40 +57,30 @@ def add_nsxv_router_binding(session, router_id, vse_id, lswitch_id, status,
             appliance_size=appliance_size,
             edge_type=edge_type)
         session.add(binding)
-        return binding
+    return binding
 
 
 def get_nsxv_router_binding(session, router_id):
-    with session.begin(subtransactions=True):
-        return (session.query(nsxv_models.NsxvRouterBinding).
-                filter_by(router_id=router_id).first())
+    return session.query(nsxv_models.NsxvRouterBinding).filter_by(
+        router_id=router_id).first()
+
+
+def get_nsxv_router_binding_by_edge(session, edge_id):
+    return session.query(nsxv_models.NsxvRouterBinding).filter_by(
+        edge_id=edge_id).first()
 
 
 def get_nsxv_router_bindings_by_edge(session, edge_id):
-    with session.begin(subtransactions=True):
-        return (session.query(nsxv_models.NsxvRouterBinding).
-                filter_by(edge_id=edge_id).all())
+    return session.query(nsxv_models.NsxvRouterBinding).filter_by(
+        edge_id=edge_id).all()
 
 
 def get_nsxv_router_bindings(session, filters=None,
                              like_filters=None):
-    with session.begin(subtransactions=True):
-        query = session.query(nsxv_models.NsxvRouterBinding)
-        return _apply_filters_to_query(
-            query, nsxv_models.NsxvRouterBinding,
-            filters, like_filters).all()
-
-
-def get_nsxv_router_bindings_with_lock(session, filters=None,
-                                       like_filters=None):
-    """Get rows of nsxv_router_bindings data with "select for update" lock.
-
-    More accurate filters mean smaller db lock range
-    """
+    session = db.get_session()
     query = session.query(nsxv_models.NsxvRouterBinding)
-    return _apply_filters_to_query(
-        query, nsxv_models.NsxvRouterBinding,
-        filters, like_filters).with_lockmode('update').all()
+    return _apply_filters_to_query(query, nsxv_models.NsxvRouterBinding,
+                                   filters, like_filters).all()
 
 
 def update_nsxv_router_binding(session, router_id, **kwargs):
@@ -99,9 +89,11 @@ def update_nsxv_router_binding(session, router_id, **kwargs):
                    filter_by(router_id=router_id).one())
         for key, value in kwargs.iteritems():
             binding[key] = value
+    return binding
 
 
 def delete_nsxv_router_binding(session, router_id):
+    session = db.get_session()
     with session.begin(subtransactions=True):
         binding = (session.query(nsxv_models.NsxvRouterBinding).
                    filter_by(router_id=router_id).first())
@@ -110,23 +102,20 @@ def delete_nsxv_router_binding(session, router_id):
 
 
 def get_edge_vnic_binding(session, edge_id, network_id):
-    with session.begin(subtransactions=True):
-        return (session.query(nsxv_models.NsxvEdgeVnicBinding).
-                filter_by(edge_id=edge_id, network_id=network_id).first())
+    return session.query(nsxv_models.NsxvEdgeVnicBinding).filter_by(
+        edge_id=edge_id, network_id=network_id).first()
 
 
 def get_edge_vnic_bindings_by_edge(session, edge_id):
     query = session.query(nsxv_models.NsxvEdgeVnicBinding)
-    query = query.filter(
+    return query.filter(
         nsxv_models.NsxvEdgeVnicBinding.edge_id == edge_id,
-        nsxv_models.NsxvEdgeVnicBinding.network_id != expr.null())
-    return query.all()
+        nsxv_models.NsxvEdgeVnicBinding.network_id != expr.null()).all()
 
 
 def get_edge_vnic_bindings_by_int_lswitch(session, lswitch_id):
-    with session.begin(subtransactions=True):
-        return (session.query(nsxv_models.NsxvEdgeVnicBinding).
-                filter_by(network_id=lswitch_id).all())
+    return session.query(nsxv_models.NsxvEdgeVnicBinding).filter_by(
+        network_id=lswitch_id).all()
 
 
 def create_edge_vnic_binding(session, edge_id, vnic_index,
@@ -138,6 +127,7 @@ def create_edge_vnic_binding(session, edge_id, vnic_index,
             tunnel_index=tunnel_index,
             network_id=network_id)
         session.add(binding)
+    return binding
 
 
 def delete_edge_vnic_binding_by_network(session, edge_id, network_id):
@@ -219,6 +209,7 @@ def allocate_edge_vnic_with_tunnel_index(session, edge_id, network_id):
             raise nsx_exc.NsxPluginException(err_msg=msg)
         binding['network_id'] = network_id
         session.add(binding)
+        session.flush()
     return binding
 
 
@@ -268,12 +259,12 @@ def create_edge_dhcp_static_binding(session, edge_id, mac_address, binding_id):
             mac_address=mac_address,
             binding_id=binding_id)
         session.add(binding)
+    return binding
 
 
 def get_edge_dhcp_static_binding(session, edge_id, mac_address):
-    with session.begin(subtransactions=True):
-        return (session.query(nsxv_models.NsxvEdgeDhcpStaticBinding).
-                filter_by(edge_id=edge_id, mac_address=mac_address).first())
+    return session.query(nsxv_models.NsxvEdgeDhcpStaticBinding).filter_by(
+        edge_id=edge_id, mac_address=mac_address).first()
 
 
 def delete_edge_dhcp_static_binding(session, edge_id, mac_address):

vmware_nsx/neutron/plugins/vmware/plugins/nsx_v_drivers/distributed_router_driver.py
@@ -14,6 +14,7 @@
 
 from oslo_utils import excutils
 
+from neutron.api.v2 import attributes as attr
 from neutron.common import exceptions as n_exc
 
 from vmware_nsx.neutron.plugins.vmware.plugins import nsx_v
@@ -75,16 +76,18 @@ class RouterDistributedDriver(router_driver.RouterBaseDriver):
 
     def update_router(self, context, router_id, router):
         gw_info = self.plugin._extract_external_gw(context, router,
-                                                   is_extract=False)
-        router_updated = super(nsx_v.NsxVPluginV2, self.plugin).update_router(
+                                                   is_extract=True)
+        super(nsx_v.NsxVPluginV2, self.plugin).update_router(
             context, router_id, router)
-        # here is used to handle routes which tenant updates.
-        if gw_info is None:
+        if gw_info != attr.ATTR_NOT_SPECIFIED:
+            self._update_router_gw_info(context, router_id, gw_info)
+        else:
+            # here is used to handle routes which tenant updates.
             router_db = self.plugin._get_router(context, router_id)
             nexthop = self.plugin._get_external_attachment_info(
                 context, router_db)[2]
             self.update_routes(context, router_id, nexthop)
-        return router_updated
+        return self.plugin.get_router(context, router_id)
 
     def delete_router(self, context, router_id):
         self.edge_manager.delete_lrouter(context, router_id, dist=True)

vmware_nsx/neutron/plugins/vmware/plugins/nsx_v_drivers/exclusive_router_driver.py
@@ -14,6 +14,8 @@
 
 from oslo_log import log as logging
 
+from neutron.api.v2 import attributes as attr
+
 from vmware_nsx.neutron.plugins.vmware.plugins import nsx_v
 from vmware_nsx.neutron.plugins.vmware.plugins.nsx_v_drivers import (
     abstract_router_driver as router_driver)
@@ -35,16 +37,18 @@ class RouterExclusiveDriver(router_driver.RouterBaseDriver):
 
     def update_router(self, context, router_id, router):
         gw_info = self.plugin._extract_external_gw(context, router,
-                                                   is_extract=False)
-        router_updated = super(nsx_v.NsxVPluginV2, self.plugin).update_router(
+                                                   is_extract=True)
+        super(nsx_v.NsxVPluginV2, self.plugin).update_router(
             context, router_id, router)
-        # here is used to handle routes which tenant updates.
-        if gw_info is None:
+        if gw_info != attr.ATTR_NOT_SPECIFIED:
+            self._update_router_gw_info(context, router_id, gw_info)
+        else:
+            # here is used to handle routes which tenant updates.
             router_db = self.plugin._get_router(context, router_id)
             nexthop = self.plugin._get_external_attachment_info(
                 context, router_db)[2]
             self.update_routes(context, router_id, nexthop)
-        return router_updated
+        return self.plugin.get_router(context, router_id)
 
     def delete_router(self, context, router_id):
         self.edge_manager.delete_lrouter(context, router_id, dist=False)

vmware_nsx/neutron/plugins/vmware/plugins/nsx_v_drivers/shared_router_driver.py
@@ -15,7 +15,7 @@
 from oslo_concurrency import lockutils
 from oslo_config import cfg
 
-from neutron import context as neutron_context
+from neutron.api.v2 import attributes as attr
 from neutron.db import l3_db
 from neutron.db import models_v2
 from oslo_log import log as logging
@@ -55,7 +55,7 @@ class RouterSharedDriver(router_driver.RouterBaseDriver):
                 super(nsx_v.NsxVPluginV2, self.plugin).update_router(
                     context, router_id, router)
             # here is used to handle routes which tenant updates.
-            if gw_info is not None:
+            if gw_info != attr.ATTR_NOT_SPECIFIED:
                 self._update_router_gw_info(context, router_id, gw_info)
             else:
                 with lockutils.lock(str(edge_id),
@@ -280,7 +280,7 @@ class RouterSharedDriver(router_driver.RouterBaseDriver):
         """
         ext_net_filters = {'router:external': [True]}
         ext_nets = self.plugin.get_networks(
-            neutron_context.get_admin_context(), filters=ext_net_filters)
+            context.elevated(), filters=ext_net_filters)
         ext_net_ids = [ext_net.get('id') for ext_net in ext_nets]
         conflict_ext_net_ids = list(set(ext_net_ids) &
                                     set(conflict_network_ids))

vmware_nsx/neutron/plugins/vmware/vshield/edge_utils.py
@@ -14,6 +14,8 @@
 #    under the License.
 
 import eventlet
+import random
+from sqlalchemy import exc as db_base_exc
 import time
 
 from oslo_concurrency import lockutils
@@ -37,6 +39,7 @@ from vmware_nsx.neutron.plugins.vmware.vshield.tasks import (
     constants as task_const)
 from vmware_nsx.neutron.plugins.vmware.vshield.tasks import tasks
 from vmware_nsx.openstack.common._i18n import _LE, _LW
+from vmware_nsx.neutron.plugins.vmware.vshield import vcns
 
 WORKER_POOL_SIZE = 8
 LOG = logging.getLogger(__name__)
@@ -190,13 +193,12 @@ class EdgeManager(object):
                 dist=(binding['edge_type'] == nsxv_constants.VDR_EDGE))
 
     def _clean_all_error_edge_bindings(self, context):
-        with context.session.begin(subtransactions=True):
-            filters = {'status': [plugin_const.ERROR]}
-            like_filters = {'router_id': vcns_const.BACKUP_ROUTER_PREFIX + "%"}
-            error_router_bindings = nsxv_db.get_nsxv_router_bindings(
-                context.session, filters=filters, like_filters=like_filters)
-            self._delete_backup_edges_on_db(context,
-                                            error_router_bindings)
+        filters = {'status': [plugin_const.ERROR]}
+        like_filters = {'router_id': vcns_const.BACKUP_ROUTER_PREFIX + "%"}
+        error_router_bindings = nsxv_db.get_nsxv_router_bindings(
+            context.session, filters=filters, like_filters=like_filters)
+        self._delete_backup_edges_on_db(context,
+                                        error_router_bindings)
         self._delete_backup_edges_at_backend(context,
                                              error_router_bindings)
 
@@ -235,27 +237,26 @@ class EdgeManager(object):
                                 edge_type=nsxv_constants.SERVICE_EDGE):
         """Check edge pool's status and return one available edge for use."""
         admin_ctx = q_context.get_admin_context()
-        with admin_ctx.session.begin(subtransactions=True):
-            backup_router_bindings = self._get_backup_edge_bindings(
-                admin_ctx, appliance_size=appliance_size, edge_type=edge_type,
-                db_update_lock=True)
-            backup_num = len(backup_router_bindings)
-            if backup_num > maximum_pooled_edges:
-                self._delete_backup_edges_on_db(
-                    admin_ctx,
-                    backup_router_bindings[:backup_num - maximum_pooled_edges])
-            elif backup_num < minimum_pooled_edges:
-                new_backup_num = backup_num
-                router_ids = []
-                while (new_backup_num < minimum_pooled_edges):
-                    router_ids.extend(
-                        self._deploy_backup_edges_on_db(
-                            admin_ctx, 1, appliance_size=appliance_size,
-                            edge_type=edge_type))
-                    new_backup_num = len(
-                        self._get_backup_edge_bindings(
-                            admin_ctx, appliance_size=appliance_size,
-                            edge_type=edge_type, db_update_lock=True))
+        backup_router_bindings = self._get_backup_edge_bindings(
+            admin_ctx, appliance_size=appliance_size, edge_type=edge_type,
+            db_update_lock=True)
+        backup_num = len(backup_router_bindings)
+        if backup_num > maximum_pooled_edges:
+            self._delete_backup_edges_on_db(
+                admin_ctx,
+                backup_router_bindings[:backup_num - maximum_pooled_edges])
+        elif backup_num < minimum_pooled_edges:
+            new_backup_num = backup_num
+            router_ids = []
+            while (new_backup_num < minimum_pooled_edges):
+                router_ids.extend(
+                    self._deploy_backup_edges_on_db(
+                        admin_ctx, 1, appliance_size=appliance_size,
+                        edge_type=edge_type))
+                new_backup_num = len(
+                    self._get_backup_edge_bindings(
+                        admin_ctx, appliance_size=appliance_size,
+                        edge_type=edge_type, db_update_lock=True))
 
         if backup_num > maximum_pooled_edges:
             self._delete_backup_edges_at_backend(
@@ -279,13 +280,23 @@ class EdgeManager(object):
                                       edge_type=nsxv_constants.SERVICE_EDGE):
         backup_router_bindings = self._get_backup_edge_bindings(
             context, appliance_size=appliance_size, edge_type=edge_type)
-        for router_binding in backup_router_bindings:
+        while backup_router_bindings:
+            router_binding = random.choice(backup_router_bindings)
             if (router_binding['status'] == plugin_const.ACTIVE):
                 if not self.check_edge_active_at_backend(
                     router_binding['edge_id']):
+                    LOG.debug("Delete unavailable backup resource "
+                              "%(router_id)s with edge_id %(edge_id)s",
+                              {'router_id': router_binding['router_id'],
+                               'edge_id': router_binding['edge_id']})
                     self._delete_edge(context, router_binding)
                 else:
+                    LOG.debug("Get an available backup resource "
+                              "%(router_id)s with edge_id %(edge_id)s",
+                              {'router_id': router_binding['router_id'],
+                               'edge_id': router_binding['edge_id']})
                     return router_binding
+            backup_router_bindings.remove(router_binding)
 
     def _get_physical_provider_network(self, context, network_id):
         phy_net = nsxv_db.get_network_bindings(context.session, network_id)
@@ -410,6 +421,7 @@ class EdgeManager(object):
                                            tunnel_index,
                                            address_groups=iface_list)
 
+    @vcns.retry_upon_exception(db_base_exc.OperationalError, max_delay=10)
     def _allocate_edge_appliance(self, context, resource_id, name,
                                  appliance_size=nsxv_constants.LARGE,
                                  dist=False):
@@ -465,6 +477,9 @@ class EdgeManager(object):
                 plugin_const.PENDING_CREATE,
                 appliance_size=appliance_size,
                 edge_type=edge_type)
+            LOG.debug("Select edge: %(edge_id)s from pool for %(name)s",
+                      {'edge_id': available_router_binding['edge_id'],
+                       'name': name})
             fake_jobdata = {
                 'context': context,
                 'router_id': lrouter['id']}
@@ -483,13 +498,12 @@ class EdgeManager(object):
                 resource_id, available_router_binding['edge_id'],
                 name, None, appliance_size=appliance_size, dist=dist)
             task.wait(task_const.TaskState.RESULT)
-        with context.session.begin(subtransactions=True):
-            backup_num = len(self._get_backup_edge_bindings(
-                context, appliance_size=appliance_size, edge_type=edge_type,
-                db_update_lock=True))
-            router_ids = self._deploy_backup_edges_on_db(
-                context, edge_pool_range['minimum_pooled_edges'] - backup_num,
-                appliance_size=appliance_size, edge_type=edge_type)
+        backup_num = len(self._get_backup_edge_bindings(
+            context, appliance_size=appliance_size, edge_type=edge_type,
+            db_update_lock=True))
+        router_ids = self._deploy_backup_edges_on_db(
+            context, edge_pool_range['minimum_pooled_edges'] - backup_num,
+            appliance_size=appliance_size, edge_type=edge_type)
         self._deploy_backup_edges_at_backend(
             context, router_ids,
             appliance_size=appliance_size, edge_type=edge_type)
@@ -749,10 +763,13 @@ class EdgeManager(object):
 
         if allocate_new_edge:
             self._allocate_dhcp_edge_appliance(context, resource_id)
-            new_edge = nsxv_db.get_nsxv_router_binding(context.session,
-                                                       resource_id)
-            nsxv_db.allocate_edge_vnic_with_tunnel_index(
-                context.session, new_edge['edge_id'], network_id)
+            with lockutils.lock('nsx-edge-pool',
+                                lock_file_prefix='edge-bind-',
+                                external=True):
+                new_edge = nsxv_db.get_nsxv_router_binding(context.session,
+                                                           resource_id)
+                nsxv_db.allocate_edge_vnic_with_tunnel_index(
+                    context.session, new_edge['edge_id'], network_id)
 
             # If a new Edge was allocated, return resource_id
             return resource_id

vmware_nsx/neutron/tests/unit/vmware/vshield/test_edge_utils.py
@@ -318,12 +318,12 @@ class EdgeManagerTestCase(EdgeUtilsTestCaseMixin):
     def test_get_available_router_bindings(self):
         appliance_size = nsxv_constants.LARGE
         edge_type = nsxv_constants.SERVICE_EDGE
-        pool_edges = (self._create_edge_pools(1, 2, 3, 4, 5) +
+        pool_edges = (self._create_edge_pools(1, 2, 3, 0, 5) +
                       self._create_edge_pools(
-                          1, 2, 3, 4, 5, size=nsxv_constants.COMPACT))
+                          1, 2, 3, 0, 5, size=nsxv_constants.COMPACT))
         self._populate_vcns_router_binding(pool_edges)
         expect_backup_bindings = self._create_backup_router_bindings(
-            1, 2, 3, 4, 5, error_status=plugin_const.ERROR)
+            1, 2, 3, 0, 5, error_status=plugin_const.ERROR)
         binding = self.edge_manager._get_available_router_binding(
             self.ctx, appliance_size=appliance_size, edge_type=edge_type)
         router_bindings = [

commit 25aa94b7ea85f6096d32490a7a544729b050be63

Address exception when creating metadata service
Commit 1b1f7a0c964f9abf74fd99bc80f98480bda574ee introduced the
issue.

Ensure that the gw_info is set correctly.

Change-Id: I1db5e4f8c9a0836ab752a8a69409cdd3796f59dd
Closes-bug: #1446661

vmware_nsx/neutron/plugins/vmware/plugins/nsx_v.py
@@ -1217,7 +1217,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
         router_driver.create_router(
             context, lrouter,
             allow_metadata=(allow_metadata and self.metadata_proxy_handler))
-        if gw_info is not None:
+        if gw_info != attr.ATTR_NOT_SPECIFIED:
             router_driver._update_router_gw_info(
                 context, lrouter['id'], gw_info)
         return self.get_router(context, lrouter['id'])