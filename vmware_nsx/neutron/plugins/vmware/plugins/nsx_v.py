6b87e86bd490578b225216c2982f6836ebcb8adf
Gary Kotton <gkotton@vmware.com>
2015-07-19 07:30:27 -0700

    NSXv: fix broken log
    Commit bf7b098788b42eb760250948f8b09c21a376c600 introduced a problematic
    log.
    
    Change-Id: Iaead99554ae4d80d5ceecd3410a389914fd42459
    (cherry picked from commit ce1f21c826a7eec11581911dab8dc2d9c4bec121)

--- vmware_nsx/neutron/plugins/vmware/plugins/nsx_v.py
+++ vmware_nsx/neutron/plugins/vmware/plugins/nsx_v.py
@@ -451,7 +451,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
             try:
                 self.nsx_v.vcns.add_member_to_security_group(
                     sg_id, vnic_id)
-                LOG.info(_LI("Added %s(sg_id)s member to NSX security "
+                LOG.info(_LI("Added %(sg_id)s member to NSX security "
                              "group %(vnic_id)s"),
                          {'sg_id': sg_id, 'vnic_id': vnic_id})
             except Exception as e:
