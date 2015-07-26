9de1a96d404a9c4c7330cf65f0c4aca7fe4c6df9
Roey Chen <roeyc@vmware.com>
2015-07-26 07:12:01 -0700

    Nsx manager client: return meaningful errors
    Change-Id: I03dc53a8aed95d6e8910027e657529170a29fbf5

--- vmware_nsx/neutron/tests/unit/vmware/nsxlib/v3/test_port.py
+++ vmware_nsx/neutron/tests/unit/vmware/nsxlib/v3/test_port.py
@@ -15,7 +15,6 @@
 #
 
 import mock
-import requests
 
 from oslo_log import log
 
@@ -28,54 +27,45 @@ LOG = log.getLogger(__name__)
 
 class NsxLibPortTestCase(nsxlib_testcase.NsxLibTestCase):
 
-    def _create_mock_object(self, fake_object):
-        """Construct mock response object"""
-        mock_response = mock.Mock()
-        if fake_object is not None:
-            mock_response.json.return_value = fake_object
-        return mock_response
-
-    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3.requests.post")
-    def test_create_logical_port(self, mock_post):
+    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3"
+                ".client.create_resource")
+    def test_create_logical_port(self, mock_create_resource):
         """
         Test creating a port returns the correct response and 200 status
         """
-        mock_post.return_value = self._create_mock_object(
-                                     test_constants_v3.FAKE_PORT)
-        mock_post.return_value.status_code = requests.codes.created
+        mock_create_resource.return_value = test_constants_v3.FAKE_PORT
 
         result = nsxlib.create_logical_port(
-                     test_constants_v3.FAKE_PORT['logical_switch_id'],
-                     test_constants_v3.FAKE_PORT['attachment']['id'],
-                     tags={})
+            test_constants_v3.FAKE_PORT['logical_switch_id'],
+            test_constants_v3.FAKE_PORT['attachment']['id'],
+            tags={})
 
         self.assertEqual(test_constants_v3.FAKE_PORT, result)
 
-    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3.requests.post")
-    def test_create_logical_port_admin_down(self, mock_post):
+    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3"
+                ".client.create_resource")
+    def test_create_logical_port_admin_down(self, mock_create_resource):
         """
         Test creating port with admin_state down
         """
         fake_port = test_constants_v3.FAKE_PORT
         fake_port['admin_state'] = "DOWN"
-        mock_post.return_value = self._create_mock_object(fake_port)
-        mock_post.return_value.status_code = requests.codes.created
+        mock_create_resource.return_value = fake_port
 
         result = nsxlib.create_logical_port(
-                     test_constants_v3.FAKE_PORT['logical_switch_id'],
-                     test_constants_v3.FAKE_PORT['attachment']['id'],
-                     tags={}, admin_state=False)
+            test_constants_v3.FAKE_PORT['logical_switch_id'],
+            test_constants_v3.FAKE_PORT['attachment']['id'],
+            tags={}, admin_state=False)
 
         self.assertEqual(fake_port, result)
 
-    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3.requests.delete")
-    def test_delete_logical_port(self, mock_delete):
+    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3"
+                ".client.delete_resource")
+    def test_delete_logical_port(self, mock_delete_resource):
         """
         Test deleting port
         """
-        mock_delete.return_value = self._create_mock_object(None)
-        mock_delete.return_value.status_code = requests.codes.ok
+        mock_delete_resource.return_value = None
 
-        result = nsxlib.delete_logical_port(
-                     test_constants_v3.FAKE_PORT['id'])
+        result = nsxlib.delete_logical_port(test_constants_v3.FAKE_PORT['id'])
         self.assertIsNone(result)
