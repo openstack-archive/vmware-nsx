abd9b627ff20fe1e5b0e1d18a3e377cd407788d6
Amey Bhide <abhide@vmware.com>
2015-08-13 14:33:04 -0700

    Adds CRD operations for NSX Qos Service Plugin
    This is an initial patch; only adds support for:
    neutron qos-policy-create
    neutron qos-policy-show
    neutron qos-policy-list
    neutron qos-policy-delete
    
    Change-Id: I01572452bd72834b73b6e4b542d70400540d1b7c

--- vmware_nsx/neutron/plugins/vmware/nsxlib/v3/__init__.py
+++ vmware_nsx/neutron/plugins/vmware/nsxlib/v3/__init__.py
@@ -143,14 +143,17 @@ def delete_logical_router_port(logical_port_id):
     client.delete_resource(resource)
 
 
-def create_qos_switching_profile(qos_marking, dscp, tags, name=None,
+def create_qos_switching_profile(tags, qos_marking=None, dscp=None, name=None,
                                  description=None):
     resource = 'switching-profiles'
     body = {"resource_type": "QosSwitchingProfile",
-            "tags": tags,
-            "dscp": {"priority": dscp,
-                     "mode": qos_marking.upper()}}
+            "tags": tags}
     # TODO(abhide): Add TrafficShaper configuration.
+    if qos_marking:
+        body["dscp"] = {}
+        body["dscp"]["mode"] = qos_marking.upper()
+        if dscp:
+            body["dscp"]["priority"] = dscp
     if name:
         body["display_name"] = name
     if description:
@@ -165,4 +168,4 @@ def get_qos_switching_profile(profile_id):
 
 def delete_qos_switching_profile(profile_id):
     resource = 'switching-profiles/%s' % profile_id
-    return client.delete_resource(resource)
+    client.delete_resource(resource)
