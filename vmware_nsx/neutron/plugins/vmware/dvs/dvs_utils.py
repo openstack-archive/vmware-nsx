957ab703c009c73cce990907e48850e17b695c6d
Eric Brown <browne@vmware.com>
2015-08-24 09:15:47 -0700

    Use min and max on IntOpt option types
    Latest version of oslo.config support the ability to set valid
    range on IntOpt option values.  This patch makes use of that feature
    for a number of options with well known minimum and maximum values.
    
    Change-Id: I4f20adbd0140e312f9cde86f71000c3dbd6aaf19

--- vmware_nsx/neutron/plugins/vmware/dvs/dvs_utils.py
+++ vmware_nsx/neutron/plugins/vmware/dvs/dvs_utils.py
@@ -19,7 +19,7 @@ dvs_opts = [
     cfg.StrOpt('host_ip',
                help='Hostname or IP address for connection to VMware vCenter '
                     'host.'),
-    cfg.IntOpt('host_port', default=443,
+    cfg.IntOpt('host_port', default=443, min=1, max=65535,
                help='Port for connection to VMware vCenter host.'),
     cfg.StrOpt('host_username',
                help='Username for connection to VMware vCenter host.'),
