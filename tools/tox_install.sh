794018067c881cda569b172e40f1038d23653838
Amey Bhide <abhide@vmware.com>
2015-10-15 14:14:26 -0700

    Add networking-l2gw to tox_install.sh
    Change-Id: Iaa8b5c2bcb14b8cad0df334726688bb1f9cd0613

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -15,27 +15,45 @@
 
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
 neutron_installed=$(echo "import neutron" | python 2>/dev/null ; echo $?)
+networking_l2gw_installed=$(echo "import networking_l2gw" | python 2>/dev/null ; echo $?)
 
 set -ex
 
 cwd=$(/bin/pwd)
+> /tmp/tox_install.txt
 
-if [ $neutron_installed -eq 0 ]; then
-    echo "ALREADY INSTALLED" > /tmp/tox_install.txt
-    echo "Neutron already installed; using existing package"
-elif [ -x "$ZUUL_CLONER" ]; then
-    echo "ZUUL CLONER" > /tmp/tox_install.txt
+zuul_cloner () {
+    echo "ZUUL CLONER" >> /tmp/tox_install.txt
     cd /tmp
     $ZUUL_CLONER --cache-dir \
         /opt/git \
-        git://git.openstack.org \
-        openstack/neutron
-    cd openstack/neutron
+        git://git.openstack.org $1
+    cd $1
     pip install -e .
     cd "$cwd"
+}
+
+pip_hardcode () {
+    echo "PIP HARDCODE: $1" >> /tmp/tox_install.txt
+    pip install -U -egit+https://git.openstack.org/$1
+}
+
+if [ $neutron_installed -eq 0 ]; then
+    echo "NEUTRON ALREADY INSTALLED" >> /tmp/tox_install.txt
+    echo "Neutron already installed; using existing package"
+elif [ -x "$ZUUL_CLONER" ]; then
+    zuul_cloner openstack/neutron
 else
-    echo "PIP HARDCODE" > /tmp/tox_install.txt
-    pip install -U -egit+https://git.openstack.org/openstack/neutron#egg=neutron
+    pip_hardcode openstack/neutron#egg=neutron
+fi
+
+if [ $networking_l2gw_installed -eq 0 ]; then
+    echo "NETWORKING_L2GW ALREADY INSTALLED" >> /tmp/tox_install.txt
+    echo "Networking-l2gw already installed; using existing package"
+elif [ -x "$ZUUL_CLONER" ]; then
+    zuul_cloner openstack/networking-l2gw
+else
+    pip_hardcode openstack/networking-l2gw#egg=networking-l2gw
 fi
 
 pip install -U $*
