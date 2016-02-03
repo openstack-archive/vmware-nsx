895c747e7dd3d17ec13b13aea0888ed627b02844
Boden R <bodenvmw@gmail.com>
2016-02-03 12:10:11 -0700

    NSX-v3 proxy exception handling
    This patch updates the NSX v3 API client cluster's connection
    error detection by delegating such detection to the underlying
    http provider. This functionality allows the cluster to better
    detect and handle when a request failed due to a true
    connection error.
    
    Unit tests are also included.
    
    backport: liberty
    
    Change-Id: Ic5f67b040eb0baf777bf7a22248266625560939e
    Closes-Bug: #1541403

--- vmware_nsx/nsxlib/v3/cluster.py
+++ vmware_nsx/nsxlib/v3/cluster.py
@@ -30,6 +30,7 @@ from oslo_config import cfg
 from oslo_log import log
 from oslo_service import loopingcall
 from requests import adapters
+from requests import exceptions as requests_exceptions
 from vmware_nsx._i18n import _, _LI, _LW
 from vmware_nsx.common import exceptions as nsx_exc
 from vmware_nsx.nsxlib.v3 import client as nsx_client
@@ -70,6 +71,13 @@ class AbstractHTTPProvider(object):
         requests.Session http methods (get(), put(), etc.).
         """
         pass
+
+    @abc.abstractmethod
+    def is_connection_exception(self, exception):
+        """Determine if the given exception is related to connection
+        failure. Return True if it's a connection exception and
+        False otherwise.
+        """
 
 
 class TimeoutSession(requests.Session):
@@ -128,6 +136,9 @@ class NSXRequestsHTTPProvider(AbstractHTTPProvider):
         session.mount('https://', adapter)
 
         return session
+
+    def is_connection_exception(self, exception):
+        return isinstance(exception, requests_exceptions.ConnectionError)
 
 
 class ClusterHealth(object):
@@ -412,6 +423,9 @@ class ClusteredAPI(object):
                 return response
             except Exception as e:
                 LOG.warning(_LW("Request failed due to: %s"), e)
+                if not self._http_provider.is_connection_exception(e):
+                    # only trap and retry connection errors
+                    raise e
                 endpoint.set_state(EndpointState.DOWN)
                 # retry until exhausting endpoints
                 return self._proxy(proxy_for, uri, *args, **kwargs)
