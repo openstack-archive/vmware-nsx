5a6aa629f879b118fdeec8f2f516ebf1c01731f8
Roey Chen <roeyc@vmware.com>
2016-03-06 05:20:33 -0800

    NSXv: Better exception handling when failing to create a secgroup rule
    Creation of a duplicate security-group rule will cause all backend rules in
    the security-group to be removed, instead, only the duplicate one which was
    created should be deleted.
    
    Closes-Bug: #1553732
    Change-Id: I7a6e21f41ee3cffaec1a1f0efd281782c4d7fc5a

--- vmware_nsx/plugins/nsx_v/plugin.py
+++ vmware_nsx/plugins/nsx_v/plugin.py
@@ -2003,7 +2003,8 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
                             context.session, neutron_rule_id, nsx_rule_id)
         except Exception:
             with excutils.save_and_reraise_exception():
-                for nsx_rule_id in [p['nsx_id'] for p in rule_pairs]:
+                for nsx_rule_id in [p['nsx_id'] for p in rule_pairs
+                                    if p['neutron_id'] in ruleids]:
                     self.nsx_v.vcns.remove_rule_from_section(
                         section_uri, nsx_rule_id)
                 LOG.exception(_("Failed to create security group rule"))
