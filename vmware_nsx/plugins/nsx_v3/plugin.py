1239cee9a003a860c9c3225a19f14601d813f7fe
Roey Chen <roeyc@vmware.com>
2016-03-10 02:58:14 -0800

    NSXv3: Avoid AttributeError in create_security_group exception handling
    Change-Id: Iba1731e1fd4d3cd2bed9f79675636771e3189270

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -1690,8 +1690,8 @@ class NsxV3Plugin(addr_pair_db.AllowedAddressPairsMixin,
             secgroup, resource_type='os-neutron-secgr-id',
             project_name=context.tenant_name)
         name = security.get_nsgroup_name(secgroup)
-        ns_group = None
-        firewall_section = None
+        ns_group = {}
+        firewall_section = {}
 
         if not default_sg:
             tenant_id = secgroup['tenant_id']
