6dab0a19ec7f6dd079f9e4de6815f4888aad0629
Tong Liu <tongl@vmware.com>
2016-03-16 18:01:46 +0000

    Tempest: Use routers_client in router test
    Router related methods have been moved from network client to
    routers client in upstream tempest. We need to change our router
    related tests to use routers_client instead.
    
    Change-Id: Ieaad0c3827180def1531d9a9e47e9cc4d64e9ea4

--- vmware_nsx_tempest/tests/nsxv3/api/test_nsx_routers.py
+++ vmware_nsx_tempest/tests/nsxv3/api/test_nsx_routers.py
@@ -23,6 +23,15 @@ CONF = config.CONF
 
 
 class NSXv3RoutersTest(base.BaseRouterTest):
+    """Test L3 Router and realization on NSX backend
+
+    When test L3 Router feature, we need to test both REST API
+    call from neutron and realization state on backend. Two tests
+    have been added in this class:
+        - Test create and update router
+        - Test delete router
+
+    """
 
     @classmethod
     def skip_checks(cls):
@@ -42,18 +51,18 @@ class NSXv3RoutersTest(base.BaseRouterTest):
     @test.idempotent_id('0e9938bc-d2a3-4a9a-a4f9-7a93ee8bb344')
     def test_create_update_nsx_router(self):
         # Create a router
-        name = data_utils.rand_name('router-')
-        router = self.create_router(name, admin_state_up=True)
+        router_name = data_utils.rand_name('router-')
+        router = self.create_router(router_name, admin_state_up=True)
         self.addCleanup(self._delete_router, router['id'])
         nsx_router = self.nsx.get_logical_router(router['name'],
                                                  router['id'])
-        self.assertEqual(router['name'], name)
+        self.assertEqual(router['name'], router_name)
         self.assertEqual(router['admin_state_up'], True)
         self.assertIsNotNone(nsx_router)
         # Update the name of router and verify if it is updated on both
         # neutron and nsx backend
-        updated_name = 'updated ' + name
-        update_body = self.client.update_router(router['id'],
+        updated_name = 'updated ' + router_name
+        update_body = self.routers_client.update_router(router['id'],
                                                 name=updated_name)
         updated_router = update_body['router']
         nsx_router = self.nsx.get_logical_router(updated_router['name'],
@@ -65,21 +74,21 @@ class NSXv3RoutersTest(base.BaseRouterTest):
     @test.idempotent_id('6f49b69c-0800-4c83-b1f8-595ae5bfeea7')
     def test_delete_nsx_router(self):
         # Create a router
-        name = data_utils.rand_name('router-')
-        router = self.create_router(name, admin_state_up=True)
+        router_name = data_utils.rand_name('router-')
+        router = self.create_router(router_name, admin_state_up=True)
         nsx_router = self.nsx.get_logical_router(router['name'],
                                                  router['id'])
-        self.assertEqual(router['name'], name)
+        self.assertEqual(router['name'], router_name)
         self.assertIsNotNone(nsx_router)
         # Delete the router and verify it is deleted on nsx backend
-        self.client.delete_router(router['id'])
+        self.routers_client.delete_router(router['id'])
         nsx_router = self.nsx.get_logical_router(router['name'],
                                                  router['id'])
         self.assertIsNone(nsx_router)
 
     def _delete_router(self, router_id):
         # Delete the router in case the test exits with any exception
-        list_body = self.client.list_routers()
+        list_body = self.routers_client.list_routers()
         for router in list_body.get('router', []):
             if router['id'] == router_id:
-                self.client.delete_router(router_id)
+                self.routers_client.delete_router(router_id)
