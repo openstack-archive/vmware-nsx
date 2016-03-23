d9f0eefafaaef48d069221d4954cc1e0ef6cd7d4
yuyangbj <yangyu@vmware.com>
2016-03-23 15:54:13 +0800

    Checking load balancer before removing router interface
    As load balancer is created on exclusive edge, and we need to check
    if lb is created or not before we want to remove the interface. But
    the code is harded for lbaas v1, we should also consider v2 after v2
    is supported.
    
    Closes-bug: #1559893
    Change-Id: Iadca720c983d9297f759fb0aadfcd9db6f6ee140

--- vmware_nsx/tests/unit/nsx_v/test_plugin.py
+++ vmware_nsx/tests/unit/nsx_v/test_plugin.py
@@ -2356,6 +2356,19 @@ class TestExclusiveRouterTestCase(L3NatTest, L3NatTestCaseBase,
                 body = self._show('routers', router_id)
                 self.assertEqual('shared', body['router']['router_type'])
 
+    def test_router_remove_interface_with_load_balancer(self):
+        with self.router() as router, self.subnet() as subnet:
+            fixed_ips = [{'subnet_id': subnet['subnet']['id']}]
+            with self.port(subnet,
+                           device_owner='neutron:LOADBALANCER',
+                           fixed_ips=fixed_ips):
+                expected_code = webob.exc.HTTPInternalServerError.code
+                self._router_interface_action('remove',
+                                              router['router']['id'],
+                                              subnet['subnet']['id'],
+                                              None,
+                                              expected_code=expected_code)
+
 
 class ExtGwModeTestCase(NsxVPluginV2TestCase,
                         test_ext_gw_mode.ExtGwModeIntTestCase):
