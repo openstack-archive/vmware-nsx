7d46fb1b218f4b6e975611b0dec306797e8899f2
Gary Kotton <gkotton@vmware.com>
2016-03-31 08:32:21 -0700

    NSX|V3: enable plugin to use existing ID's
    Instead of generating a new ID, check if one exists, and if
    so use that one.
    
    This is done for routers and security groups
    
    Change-Id: I8c07efa33aefd9845defad728037fc66360fb03c

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -1291,7 +1291,8 @@ class NsxV3Plugin(addr_pair_db.AllowedAddressPairsMixin,
     def create_router(self, context, router):
         # TODO(berlin): admin_state_up support
         gw_info = self._extract_external_gw(context, router, is_extract=True)
-        router['router']['id'] = uuidutils.generate_uuid()
+        router['router']['id'] = (router['router'].get('id') or
+                                  uuidutils.generate_uuid())
         tags = utils.build_v3_tags_payload(
             router['router'], resource_type='os-neutron-router-id',
             project_name=context.tenant_name)
@@ -1713,7 +1714,7 @@ class NsxV3Plugin(addr_pair_db.AllowedAddressPairsMixin,
 
     def create_security_group(self, context, security_group, default_sg=False):
         secgroup = security_group['security_group']
-        secgroup['id'] = uuidutils.generate_uuid()
+        secgroup['id'] = secgroup.get('id') or uuidutils.generate_uuid()
 
         tags = utils.build_v3_tags_payload(
             secgroup, resource_type='os-neutron-secgr-id',
