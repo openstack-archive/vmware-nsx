e12a49e89a4109e4b2caa4835e3f8b9ac98cd8f5
Kobi Samoray <ksamoray@vmware.com>
2016-08-02 14:59:12 +0300

    NSXv: eliminate task use from edge update
    Edge update operation should not use task mechanism.
    
    Change-Id: I04fb392a2f44f0aa6948e8ff02057970fe934970

--- vmware_nsx/plugins/nsx_v/vshield/edge_utils.py
+++ vmware_nsx/plugins/nsx_v/vshield/edge_utils.py
@@ -610,20 +610,15 @@ class EdgeManager(object):
                        'name': name})
             edge_id = available_router_binding['edge_id']
             with locking.LockManager.get_lock(str(edge_id)):
-                jobdata = {
-                    'context': context,
-                    'router_id': lrouter['id']}
                 self.nsxv_manager.callbacks.complete_edge_creation(
                     context, edge_id, lrouter['name'], lrouter['id'], dist,
                     True)
 
                 # change edge's name at backend
-                task = self.nsxv_manager.update_edge(
-                    resource_id, available_router_binding['edge_id'],
+                self.nsxv_manager.update_edge(
+                    context, resource_id, available_router_binding['edge_id'],
                     name, None, appliance_size=appliance_size, dist=dist,
-                    jobdata=jobdata, set_errors=True,
-                    availability_zone=availability_zone)
-                task.wait(task_const.TaskState.RESULT)
+                    set_errors=True, availability_zone=availability_zone)
 
         backup_num = len(self._get_backup_edge_bindings(
             context, appliance_size=appliance_size, edge_type=edge_type,
@@ -694,11 +689,10 @@ class EdgeManager(object):
         # collect the edge to pool if pool not full
         if backup_num < edge_pool_range['maximum_pooled_edges']:
             # change edge's name at backend
-            task = self.nsxv_manager.update_edge(
-                backup_router_id, edge_id, backup_router_id, None,
+            update_result = self.nsxv_manager.update_edge(
+                context, backup_router_id, edge_id, backup_router_id, None,
                 appliance_size=binding['appliance_size'], dist=dist,
                 availability_zone=availability_zone)
-            task.wait(task_const.TaskState.RESULT)
 
             # Clean all edge vnic bindings
             nsxv_db.clean_edge_vnic_binding(context.session, edge_id)
@@ -706,7 +700,7 @@ class EdgeManager(object):
             if not dist and edge_id:
                 nsxv_db.init_edge_vnic_binding(context.session, edge_id)
 
-            if task.status == task_const.TaskStatus.COMPLETED:
+            if update_result:
                 nsxv_db.update_nsxv_router_binding(
                     context.session, backup_router_id,
                     status=plugin_const.ACTIVE)
@@ -2274,10 +2268,9 @@ class NsxVCallbacks(object):
                 nsxv_db.clean_edge_vnic_binding(
                     context.session, edge_id)
 
-    def edge_update_result(self, task):
-        router_id = task.userdata.get('router_id')
-        edge_id = task.userdata.get('edge_id')
-        if task.status == task_const.TaskStatus.COMPLETED:
+    def complete_edge_update(
+            self, context, edge_id, router_id, successful, set_errors):
+        if successful:
             LOG.debug("Successfully updated %(edge_id)s for router "
                       "%(router_id)s",
                       {'edge_id': edge_id,
@@ -2292,12 +2285,9 @@ class NsxVCallbacks(object):
                 nsxv_db.update_nsxv_router_binding(
                     admin_ctx.session, router_id,
                     status=plugin_const.ERROR)
-            if (task.userdata.get('set_errors') and
-                'jobdata' in task.userdata and
-                'context' in task.userdata['jobdata']):
+            if set_errors and context:
                 # Set the router status to ERROR
                 try:
-                    context = task.userdata['jobdata']['context']
                     router_db = self.plugin._get_router(context, router_id)
                     router_db['status'] = plugin_const.ERROR
                 except l3.RouterNotFound:
