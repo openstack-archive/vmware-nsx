49286370aec78acac9dc20025a12f6899c7d298d
linb <linb@vmware.com>
2016-09-01 01:38:17 +0000

    NSX|V3: Fix connected routes not advertised
    When disabling snat flag of router, we would consider it a NOSNAT user
    case. For this case, we need to advertise connected route so that the
    internal networks of router would be accessible from outside network.
    
    Change-Id: I6f92384bd19563ac4f9e812765a4a9a93e0c2087

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -1223,10 +1223,9 @@ class NsxV3Plugin(addr_pair_db.AllowedAddressPairsMixin,
             # TODO(berlin): bgp announce on new tier0 router
             pass
 
-        if remove_snat_rules or add_snat_rules:
-            self._routerlib.update_advertisement(nsx_router_id,
-                                           advertise_route_nat_flag,
-                                           advertise_route_connected_flag)
+        self._routerlib.update_advertisement(nsx_router_id,
+                                       advertise_route_nat_flag,
+                                       advertise_route_connected_flag)
 
     def create_router(self, context, router):
         # TODO(berlin): admin_state_up support
