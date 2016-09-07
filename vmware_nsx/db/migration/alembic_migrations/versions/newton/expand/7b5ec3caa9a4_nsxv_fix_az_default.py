0baaca9925c314476dede7bb6119eb6012e76b66
Adit Sarfaty <asarfaty@vmware.com>
2016-09-07 11:56:51 +0300

    NSX|V fix router_binding az default value after migration
    Previous newton migration left the availability zone column in the router
    bindings table empty instead of setting it to 'default'.
    This causes the old backup edged to never be used and a new pool to be created.
    
    Change-Id: Ie797bf472afda5bd38ecfceb7ebff937c7e08b08
    (cherry picked from commit e2ba869a41827ee9745d72cd4d788091bb5dd501)

---
+++ vmware_nsx/db/migration/alembic_migrations/versions/newton/expand/7b5ec3caa9a4_nsxv_fix_az_default.py
@@ -0,0 +1,33 @@
+# Copyright 2016 VMware, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Fix the availability zones default value in the router bindings table
+
+Revision ID: 7b5ec3caa9a4
+Revises: 6e6da8296c0e
+Create Date: 2016-09-07 11:38:35.369938
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '7b5ec3caa9a4'
+down_revision = '6e6da8296c0e'
+
+from alembic import op
+
+
+def upgrade():
+    #previous migration left this column empty instead of 'default'
+    op.execute("UPDATE nsxv_router_bindings SET availability_zone='default' "
+               "where availability_zone is NULL")
