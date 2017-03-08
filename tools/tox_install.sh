dde3725bd3053a9333498de1ca0fa0cb33403c72
Gary Kotton <gkotton@vmware.com>
2017-03-07 22:28:14 -0800

    Remove unconstrained for vmware-nsxlib
    This is due to the fact that we no longer use the requirements.
    This was done in commit 34568342ce37586cff9fe3fe462d3e722a4d4c04.
    
    From now one we will have a specific version that matches the
    vmware-nsx version.
    
    Change-Id: I528194276a39c9434d60312f13e358afab1c404b

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -8,8 +8,7 @@ ${DIR}/tox_install_project.sh networking-l2gw networking_l2gw $*
 ${DIR}/tox_install_project.sh networking-sfc networking_sfc $*
 ${DIR}/tox_install_project.sh neutron-lbaas neutron_lbaas $*
 ${DIR}/tox_install_project.sh tap-as-a-service neutron_taas $*
-#garyk(TODO) remove the unconstrained when requirements are updated
-${DIR}/tox_install_project.sh vmware-nsxlib vmware_nsxlib unconstrained $*
+${DIR}/tox_install_project.sh vmware-nsxlib vmware_nsxlib $*
 
 CONSTRAINTS_FILE=$1
 shift
