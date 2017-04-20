fdf7d0cf38f3db94867f90700cc6a47161f79460
Vijay Kankatala <vkankatala@vmware.com>
2017-04-20 16:03:23 -0700

    Tempest:Deploy and Validate Neutron resources using HEAT Template on NSXT|V
        The script loads the neutron resources from template and
        validates successful deployment of all resources.The script also
        does same network and crossnetwork connectivity checks.
    
        -Add HEAT package support for NSX v|t
    
    Change-Id: Ic56a2d34084b77ebb0afd5f8b149498da3238c53

--- vmware_nsx_tempest/lib/heat.py
+++ vmware_nsx_tempest/lib/heat.py
@@ -22,60 +22,71 @@ from oslo_log import log as logging
 from tempest.api.orchestration import base
 from tempest.common.utils import data_utils
 from tempest import config
-from tempest.lib import decorators
 from tempest.scenario import manager
-from tempest import test
 
+from vmware_nsx_tempest.services import nsxv3_client
 from vmware_nsx_tempest.services import nsxv_client
-
 
 CONF = config.CONF
 
 LOG = logging.getLogger(__name__)
-DIR_PATH = '/opt/stack/vmware-nsx/vmware_nsx_tempest/tests/'
 
 
 class HeatSmokeTest(base.BaseOrchestrationTest,
                     manager.NetworkScenarioTest):
+    """
+       Deploy and Test Neutron Resources using HEAT.
 
-    """Deploy and Test Neutron Resources using HEAT.
-
-       The script load the neutron resources from template and fully
-    validates successful deployment of all resources from the template.
-    The template consists of two toplogies with Shared and Exclusive router.
-    Tests will be common to toplogies (pls refer template for topo info)and
-    will be as below :
-            1. verify created resources from template
-            2. verify all created resouces from template
-              -->neutronDB-->NSXbackend
-            3. check same network connectivity
-            4. check cross network connectivity
     """
 
-    def setUp(self):
-        super(HeatSmokeTest, self).setUp()
-        self.external_network = CONF.scenario.outside_world_servers
+    @classmethod
+    def setup_clients(cls):
+        super(HeatSmokeTest, cls).setup_clients()
+        cls.routers_client = cls.os.routers_client
+        cls.backend = CONF.network.backend
+        if cls.backend == 'nsxv3':
+            cls.filename = 'nsxt_neutron_smoke'
+            cls.nsx = nsxv3_client.NSXV3Client(CONF.nsxv3.nsx_manager,
+                                               CONF.nsxv3.nsx_user,
+                                               CONF.nsxv3.nsx_password)
+        elif cls.backend == 'nsxv':
+            cls.filename = 'nsxv_neutron_smoke'
+            manager_ip = re.search(r"(\d{1,3}\.){3}\d{1,3}",
+                                   CONF.nsxv.manager_uri).group(0)
+            cls.vsm = nsxv_client.VSMClient(
+                manager_ip, CONF.nsxv.user, CONF.nsxv.password)
+
+    @classmethod
+    def setup_credentials(cls):
+        cls.set_network_resources()
+        super(HeatSmokeTest, cls).setup_credentials()
 
     @classmethod
     def read_template(cls, name, ext='yaml'):
-        loc = ["templates", "%s.%s" % (name, ext)]
-        filepath = os.path.join(DIR_PATH, *loc)
+        loc = ["tests", "templates", "%s.%s" % (name, ext)]
+        dir_path = os.path.dirname(__file__).split('/')
+        dir_path.pop()
+        dir_path = '/'.join(dir_path)
+        filepath = os.path.join(dir_path, *loc)
         if os.path.isfile(filepath):
             with open(filepath, "r") as f:
                 content = f.read()
                 return content
         else:
-            raise IOError("File %s not found " % filepath)
+            raise IOError
 
     @classmethod
     def load_template(cls, name, ext='yaml'):
-        loc = ["templates", "%s.%s" % (name, ext)]
-        filepath = os.path.join(DIR_PATH, *loc)
+        loc = ["tests", "templates", "%s.%s" % (name, ext)]
+        dir_path = os.path.dirname(__file__).split('/')
+        dir_path.pop()
+        dir_path = '/'.join(dir_path)
+        filepath = os.path.join(dir_path, *loc)
         if os.path.isfile(filepath):
             with open(filepath, "r") as f:
                 return yaml.safe_load(f)
         else:
-            raise IOError("File %s not found " % filepath)
+            raise IOError
 
     @classmethod
     def resource_setup(cls):
@@ -83,11 +94,11 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
         cls.stack_name = data_utils.rand_name('heat')
         try:
             cls.neutron_basic_template = cls.load_template(
-                'nsxv_neutron_smoke')
-            template = cls.read_template('nsxv_neutron_smoke')
+                cls.filename)
+            template = cls.read_template(cls.filename)
         except IOError as e:
-            LOG.exception(("file nsxv_neutron_smoke.yaml not found %(rsp)s") %
-                          {'rsp': e})
+            LOG.exception(("file %(rsp)s not found %(rsp1)s") %
+                          {'rsp': cls.filename, 'rsp1': e})
         cls.stack_identifier = cls.create_stack(cls.stack_name, template)
         cls.client.wait_for_stack_status(cls.stack_identifier,
                                          'CREATE_COMPLETE')
@@ -98,21 +109,6 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
         for resource in cls.resources:
             cls.test_resources[resource['logical_resource_id']] = resource
 
-    @classmethod
-    def setup_credentials(cls):
-        cls.set_network_resources()
-        super(HeatSmokeTest, cls).setup_credentials()
-
-    @classmethod
-    def setup_clients(cls):
-        super(HeatSmokeTest, cls).setup_clients()
-        cls.routers_client = cls.os.routers_client
-        cls.subnets_client = cls.os.subnets_client
-        manager_ip = re.search(r"(\d{1,3}\.){3}\d{1,3}",
-                               CONF.nsxv.manager_uri).group(0)
-        cls.vsm = nsxv_client.VSMClient(
-            manager_ip, CONF.nsxv.user, CONF.nsxv.password)
-
     def _resource_list_check(self, resource):
         # sorts out the resources and returns resource id
         if resource == 'networks':
@@ -121,7 +117,6 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
         elif resource == 'routers':
             body = self.routers_client.list_routers()
             component = 'OS::Neutron::Router'
-            print(body)
         elif resource == 'servers':
             body = self.servers_client.list_servers()
             component = 'OS::Nova::Server'
@@ -157,9 +152,7 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
                               {'dest': remote_ip, 'src': floating_ip})
                 raise
 
-    @decorators.idempotent_id('f693a425-b018-4cde-96ab-cdd5b858e15c')
-    @test.attr(type=["smoke"])
-    def test_created_resources(self):
+    def check_created_resources(self):
         """Verifies created resources from template ."""
         for resource in self.resources:
             msg = 'resource %s not create successfully' \
@@ -168,35 +161,40 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
                              msg)
             self.assertIsInstance(resource, dict)
 
-    @decorators.idempotent_id('3c3ccfcb-e50b-4372-82dc-d5b473acd506')
-    @test.attr(type=["smoke"])
-    def test_created_network(self):
+    def check_created_network(self):
         """Verifies created neutron networks."""
         network_id_list = self._resource_list_check(resource='networks')
         for network_id in network_id_list:
             body = self.networks_client.show_network(network_id)
-            self.assertEqual('True', str(body['network']['admin_state_up']))
+            self.assertEqual('True', str(body['network']
+                                         ['admin_state_up']))
             msg = 'newtwork %s not found' % body['network']['name']
-            self.assertIsNotNone(self.vsm.get_logical_switch(network_id), msg)
+            if self.backend == 'nsxv3':
+                self.assertIsNotNone(self.nsx.get_logical_switch(
+                    body['network']['name'], body['network']['id']), msg)
+            elif self.backend == 'nsxv':
+                self.assertIsNotNone(self.vsm.get_logical_switch(network_id),
+                                     msg)
 
-    @decorators.idempotent_id('b3b103a7-69b2-42ea-a1b8-aa11cc551df9')
-    @test.attr(type=["smoke"])
-    def test_created_router(self):
+    def check_created_router(self):
         """Verifies created router."""
         router_id_list = self._resource_list_check(resource='routers')
         for router_id in router_id_list:
             body = self.routers_client.show_router(router_id)
             self.assertEqual('True', str(body['router']['admin_state_up']))
-            if (body['router']['router_type']) != 'shared':
-                router_edge_name = "%s-%s" % (
-                    body['router']['name'], body['router']['id'])
-                exc_edge = self.vsm.get_edge(router_edge_name)
-                msg = 'exc edge %s not found' % body['router']['name']
-                self.assertTrue(exc_edge is not None, msg)
+            if self.backend == 'nsxv3':
+                msg = 'router %s not found' % body['router']['name']
+                self.assertIsNotNone(self.nsx.get_logical_router(
+                    body['router']['name'], body['router']['id']), msg)
+            elif self.backend == 'nsxv':
+                if (body['router']['router_type']) != 'shared':
+                    router_edge_name = "%s-%s" % (
+                        body['router']['name'], body['router']['id'])
+                    exc_edge = self.vsm.get_edge(router_edge_name)
+                    msg = 'exc edge %s not found' % body['router']['name']
+                    self.assertTrue(exc_edge is not None, msg)
 
-    @decorators.idempotent_id('2b29dfef-6d9f-4a70-9377-af432100ef10')
-    @test.attr(type=["smoke"])
-    def test_created_server(self):
+    def check_created_server(self):
         """Verifies created sever."""
         server_id_list = self._resource_list_check(resource='servers')
         for server_id in server_id_list:
@@ -204,24 +202,20 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
             msg = 'server %s not active ' % (server)
             self.assertEqual('ACTIVE', str(server['status']), msg)
 
-    @decorators.idempotent_id('d937a607-aa5e-4cf1-bbf9-00044cbe7190')
-    @test.attr(type=["smoke"])
-    def test_topo1_same_network_connectivity_(self):
+    def check_topo1_same_network_connectivity(self):
         """Verifies same network connnectivity for Topology 1 """
         address_list = []
-        topo1_server1_floatingip = self.get_stack_output(self.stack_identifier,
-                                                   'topo1_server1_floatingip')
-        server4_private_ip = self.get_stack_output(self.stack_identifier,
-                                                   'topo1_server4_private_ip')
+        topo1_server1_floatingip = self.get_stack_output(
+            self.stack_identifier, 'topo1_server1_floatingip')
+        server4_private_ip = self.get_stack_output(
+            self.stack_identifier, 'topo1_server4_private_ip')
         address_list.append(server4_private_ip)
-        LOG.info((" floating ip :%(rsp)s and private ip list : %(rsp1)s") %
+        LOG.info(" floating ip :%(rsp)s and private ip list : %(rsp1)s" %
                  {"rsp": topo1_server1_floatingip, "rsp1": address_list})
         self._check_server_connectivity(topo1_server1_floatingip, address_list,
                                         should_connect=True)
 
-    @decorators.idempotent_id('fdbc8b1a-755a-4b37-93e7-0a268e422f05')
-    @test.attr(type=["smoke"])
-    def test_topo1_cross_network_connectivity(self):
+    def check_topo1_cross_network_connectivity(self):
         """Verifies cross network connnectivity for Topology 1 """
         address_list = []
         topo1_server1_floatingip = self.get_stack_output(
@@ -232,21 +226,19 @@ class HeatSmokeTest(base.BaseOrchestrationTest,
                                                    'topo1_server3_private_ip')
         address_list.append(server2_private_ip)
         address_list.append(server3_private_ip)
-        LOG.info(("floating ip :%(rsp)s and private ip list : %(rsp1)s") %
+        LOG.info("floating ip :%(rsp)s and private ip list : %(rsp1)s" %
                  {"rsp": topo1_server1_floatingip, "rsp1": address_list})
         self._check_server_connectivity(topo1_server1_floatingip, address_list,
                                         should_connect=True)
 
-    @decorators.idempotent_id('bcefd117-c55e-499d-a34b-653b8981f1c5')
-    @test.attr(type=["smoke"])
-    def test_topo1_cross_external_connectivity(self):
+    def check_topo1_external_connectivity(self):
         """Verifies external network connnectivity for Topology 1 """
         address_list = []
-        topo1_server1_floatingip = self.get_stack_output(self.stack_identifier,
-                                                   'topo1_server1_floatingip')
+        topo1_server1_floatingip = self.get_stack_output(
+            self.stack_identifier, 'topo1_server1_floatingip')
         external_network = self.external_network[0]
         address_list.append(external_network)
-        LOG.info(("floating ip :%(rsp)s and external ip : %(rsp1)s") %
+        LOG.info("floating ip :%(rsp)s and external ip : %(rsp1)s" %
                  {"rsp": topo1_server1_floatingip, "rsp1": address_list})
-        self._check_server_connectivity(topo1_server1_floatingip, address_list,
-                                        should_connect=True)
+        self._check_server_connectivity(topo1_server1_floatingip,
+                                        address_list, should_connect=True)
