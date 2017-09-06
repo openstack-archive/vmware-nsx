ee2cfb9a5c451f6c67b0226e62504d4581a37df9
Tong Liu <tongl@vmware.com>
2017-09-06 14:26:38 -0700

    NSXv3: Fix L7 rule create/delete base on latest API
    The platform side finalized the API for LbRule including
    LbRuleAction, LbRuleCondition and LbRuleMatchType. Also it enforces
    LB rule deletion that the rule cannot be deleted with if it is
    still attached to a virtual server. This patch fix these based on
    platform changes.
    
    Change-Id: Id3709c480eb92a7d369851248f302267da0a64ef
    (cherry picked from commit 2c55108ef44932523b3813a3e639a885c8c56979)

--- vmware_nsx/services/lbaas/nsx_v3/l7policy_mgr.py
+++ vmware_nsx/services/lbaas/nsx_v3/l7policy_mgr.py
@@ -18,7 +18,9 @@ from oslo_log import helpers as log_helpers
 from oslo_log import log as logging
 
 from vmware_nsx._i18n import _
+from vmware_nsx.db import db as nsx_db
 from vmware_nsx.services.lbaas import base_mgr
+from vmware_nsxlib.v3 import exceptions as nsxlib_exc
 
 LOG = logging.getLogger(__name__)
 
@@ -50,4 +52,30 @@ class EdgeL7PolicyManager(base_mgr.Nsxv3LoadbalancerBaseManager):
 
     @log_helpers.log_method_call
     def delete(self, context, policy):
-        self._l7policy_action(context, policy, 'delete', delete=True)
+        lb_id = policy.listener.loadbalancer_id
+        vs_client = self.core_plugin.nsxlib.load_balancer.virtual_server
+        rule_client = self.core_plugin.nsxlib.load_balancer.rule
+        for rule in policy.rules:
+            binding = nsx_db.get_nsx_lbaas_l7rule_binding(
+                context.session, lb_id, policy.id, rule.id)
+            if binding:
+                vs_id = binding['lb_vs_id']
+                rule_id = binding['lb_rule_id']
+                try:
+                    # Update virtual server to remove lb rule
+                    vs_client.remove_rule(vs_id, rule_id)
+                    rule_client.delete(rule_id)
+                except nsxlib_exc.ResourceNotFound:
+                    LOG.warning('LB rule %(rule)s is not found on NSX',
+                                {'rule': rule_id})
+                except nsxlib_exc.ManagerError:
+                    self.lbv2_driver.l7policy.failed_completion(
+                        context, policy)
+                    msg = (_('Failed to delete lb rule: %(rule)s') %
+                           {'rule': rule.id})
+                    raise n_exc.BadRequest(resource='lbaas-l7rule-delete',
+                                           msg=msg)
+                nsx_db.delete_nsx_lbaas_l7rule_binding(
+                    context.session, lb_id, policy.id, rule.id)
+        self.lbv2_driver.l7policy.successful_completion(
+            context, policy, delete=True)
