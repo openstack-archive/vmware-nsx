767bfa70d796f7a7f72e13f84df196604865218e
Boden R <bodenvmw@gmail.com>
2017-10-24 15:07:59 -0600

    use extra route api def from lib
    Commit I0425384d5fd6864f336bd7a5c9a5e70f2b3568ff rehomed the extra route
    extension's API defintion to neutron-lib and
    Iae3bd4f15e12c401e615cc06358b376b81b53dc5 will consume it in neutron.
    This patch consumes it updating the code to use lib's implementation.
    
    Change-Id: I4e4dd26d01bf659eded9adf610047ea0dcec0674

--- vmware_nsx/tests/unit/nsx_v3/test_plugin.py
+++ vmware_nsx/tests/unit/nsx_v3/test_plugin.py
@@ -13,15 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
+
 import mock
 import netaddr
-import six
-from webob import exc
-
 from neutron.api.v2 import attributes
 from neutron.db import models_v2
 from neutron.extensions import address_scope
-from neutron.extensions import extraroute
 from neutron.extensions import l3
 from neutron.extensions import l3_ext_gw_mode
 from neutron.extensions import securitygroup as secgrp
@@ -35,9 +33,9 @@ from neutron.tests.unit.extensions \
     import test_l3_ext_gw_mode as test_ext_gw_mode
 from neutron.tests.unit.scheduler \
     import test_dhcp_agent_scheduler as test_dhcpagent
-
 from neutron_lib.api.definitions import address_scope as addr_apidef
 from neutron_lib.api.definitions import external_net as extnet_apidef
+from neutron_lib.api.definitions import extraroute as xroute_apidef
 from neutron_lib.api.definitions import port_security as psec
 from neutron_lib.api.definitions import portbindings
 from neutron_lib.api.definitions import provider_net as pnet
@@ -48,6 +46,7 @@ from neutron_lib import exceptions as n_exc
 from neutron_lib.plugins import directory
 from oslo_config import cfg
 from oslo_utils import uuidutils
+from webob import exc
 
 from vmware_nsx.api_client import exception as api_exc
 from vmware_nsx.common import utils
@@ -964,7 +963,7 @@ class TestL3ExtensionManager(object):
             l3.RESOURCE_ATTRIBUTE_MAP[key].update(
                 l3_ext_gw_mode.EXTENDED_ATTRIBUTES_2_0.get(key, {}))
             l3.RESOURCE_ATTRIBUTE_MAP[key].update(
-                extraroute.EXTENDED_ATTRIBUTES_2_0.get(key, {}))
+                xroute_apidef.RESOURCE_ATTRIBUTE_MAP.get(key, {}))
         # Finally add l3 resources to the global attribute map
         attributes.RESOURCE_ATTRIBUTE_MAP.update(
             l3.RESOURCE_ATTRIBUTE_MAP)
@@ -980,29 +979,18 @@ class TestL3ExtensionManager(object):
         return []
 
 
-def backup_l3_attribute_map():
-    """Return a backup of the original l3 attribute map."""
-    return dict((res, attrs.copy()) for
-                (res, attrs) in six.iteritems(l3.RESOURCE_ATTRIBUTE_MAP))
-
-
-def restore_l3_attribute_map(map_to_restore):
-    """Ensure changes made by fake ext mgrs are reverted."""
-    l3.RESOURCE_ATTRIBUTE_MAP = map_to_restore
-
-
 class L3NatTest(test_l3_plugin.L3BaseForIntTests, NsxV3PluginTestCaseMixin,
                 test_address_scope.AddressScopeTestCase):
 
-    def _restore_l3_attribute_map(self):
-        l3.RESOURCE_ATTRIBUTE_MAP = self._l3_attribute_map_bk
+    def _restore(self):
+        l3.RESOURCE_ATTRIBUTE_MAP = self._backup
 
     def setUp(self, plugin=PLUGIN_NAME, ext_mgr=None,
               service_plugins=None):
-        self._l3_attribute_map_bk = backup_l3_attribute_map()
+        self._backup = copy.deepcopy(l3.RESOURCE_ATTRIBUTE_MAP)
         cfg.CONF.set_override('api_extensions_path', vmware.NSXEXT_PATH)
         cfg.CONF.set_default('max_routes', 3)
-        self.addCleanup(restore_l3_attribute_map, self._l3_attribute_map_bk)
+        self.addCleanup(self._restore)
         ext_mgr = ext_mgr or TestL3ExtensionManager()
         mock_nsx_version = mock.patch.object(nsx_plugin.utils,
                                              'is_nsx_version_2_0_0',
