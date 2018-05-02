openstack-archive/vmware-nsx

commit a97627ac513c1775c656fdb4b6acdec8b4de2f05

NSX|V3: only allow physical network to be confogured for external net
External network type or segmentation ID is not support so we throw
an exception if one of these is configured at external network
creation.

An external network is just associated to a edge cluster.

Change-Id: I0a7f265f4af4be9a37169fe07cefe574f49d4401

vmware_nsx/plugins/nsx_v3/plugin.py
@@ -901,6 +901,11 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
             tier0_uuid = self._default_tier0_router
         else:
             tier0_uuid = net_data[pnet.PHYSICAL_NETWORK]
+        if ((validators.is_attr_set(net_data.get(pnet.NETWORK_TYPE)) and
+             net_data.get(pnet.NETWORK_TYPE) != utils.NetworkTypes.L3_EXT) or
+            validators.is_attr_set(net_data.get(pnet.SEGMENTATION_ID))):
+            msg = _("Invalid provider network configuration")
+            raise n_exc.InvalidInput(error_message=msg)
         self.nsxlib.router.validate_tier0(self.tier0_groups_dict, tier0_uuid)
         return (True, utils.NetworkTypes.L3_EXT, tier0_uuid, 0)
 

commit 9c52c029d55916a95692fb1286e3cef57e7fd8f7

NSX|V3: support 'local' type for external network
Commit a97627ac513c1775c656fdb4b6acdec8b4de2f05 caused a
degradation with external network creation. Prior to this
patch a external network could be created in horizon with
type 'local.

That support needs to be continued for backwards compatibility.

Change-Id: Id0e891842c1d4e63c522a82b09beeeeca6fdbb7b

vmware_nsx/common/utils.py
@@ -51,6 +51,7 @@ class NetworkTypes(object):
     VLAN = 'vlan'
     BRIDGE = 'bridge'
     PORTGROUP = 'portgroup'
+    LOCAL = 'local'
 
 
 # Allowed network types for the NSX-v Plugin

vmware_nsx/plugins/nsx_v3/plugin.py
@@ -954,7 +954,8 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
         else:
             tier0_uuid = net_data[pnet.PHYSICAL_NETWORK]
         if ((validators.is_attr_set(net_data.get(pnet.NETWORK_TYPE)) and
-             net_data.get(pnet.NETWORK_TYPE) != utils.NetworkTypes.L3_EXT) or
+             net_data.get(pnet.NETWORK_TYPE) != utils.NetworkTypes.L3_EXT and
+             net_data.get(pnet.NETWORK_TYPE) != utils.NetworkTypes.LOCAL) or
             validators.is_attr_set(net_data.get(pnet.SEGMENTATION_ID))):
             msg = _("Invalid provider network configuration")
             raise n_exc.InvalidInput(error_message=msg)