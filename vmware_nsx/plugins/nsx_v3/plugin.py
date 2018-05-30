3d7b0897ff4a08b1bf918599f845f24a61acbc85
Adit Sarfaty <asarfaty@vmware.com>
2018-05-30 10:29:44 +0300

    NSX|v3: Do not retry on DB duplications on section init
    When initialzing the default NSX section and NS group, the plugin
    create a default global security group, in order to make sure no
    other server is doing it at the same time.
    In case anotehr server does, we get a DP duplicated entry error,
    but after retry it somehow works, causing the 2 process to handle
    the backend at the same time.
    This patch avoids the retry in case of DB errors for this case.
    
    Change-Id: I0775e284f21012a01298168d5ca3dd1c74b95f05

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -15,6 +15,7 @@
 import copy
 import time
 
+import mock
 import netaddr
 from neutron_lib.api.definitions import allowedaddresspairs as addr_apidef
 from neutron_lib.api.definitions import availability_zone as az_def
@@ -349,9 +350,12 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
                           'name': 'NSX Internal',
                           'description': ''}}
             try:
-                # ensure that the global default is created
-                super(NsxV3Plugin, self).create_security_group(
-                    context, sec_group, True)
+                # ensure that the global default is created, and only once
+                # without retrying on DB errors
+                with mock.patch("neutron.db.api.is_retriable",
+                                return_value=False):
+                    super(NsxV3Plugin, self).create_security_group(
+                        context, sec_group, True)
             except Exception:
                 # Treat a race of multiple processing creating the sec group
                 LOG.warning('Unable to create global security group. Probably '
@@ -3852,8 +3856,6 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
     def _update_router_wrapper(self, context, router_id, router):
         if cfg.CONF.api_replay_mode:
-            # Only import mock if the reply mode is used
-            import mock
             # NOTE(arosen): the mock.patch here is needed for api_replay_mode
             with mock.patch("neutron.plugins.common.utils._fixup_res_dict",
                             side_effect=api_replay_utils._fixup_res_dict):
@@ -4183,8 +4185,6 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
     def _add_router_interface_wrapper(self, context, router_id,
                                       interface_info):
         if cfg.CONF.api_replay_mode:
-            # Only import mock if the reply mode is used
-            import mock
             # NOTE(arosen): the mock.patch here is needed for api_replay_mode
             with mock.patch("neutron.plugins.common.utils._fixup_res_dict",
                             side_effect=api_replay_utils._fixup_res_dict):
@@ -4404,8 +4404,6 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
     def _create_floating_ip_wrapper(self, context, floatingip):
         if cfg.CONF.api_replay_mode:
-            # Only import mock if the reply mode is used
-            import mock
             # NOTE(arosen): the mock.patch here is needed for api_replay_mode
             with mock.patch("neutron.plugins.common.utils._fixup_res_dict",
                             side_effect=api_replay_utils._fixup_res_dict):
