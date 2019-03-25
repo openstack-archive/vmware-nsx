b6745677f3950b05707b9d2bd51e49e6f918b58b
Adit Sarfaty <asarfaty@vmware.com>
2019-03-25 11:04:10 +0200

    NSX|V3: remove redundent code in get_port/s
    In get_port and get ports the plugins used _get_port to get each of the
    ports, and run the apply_funcs.
    Since this cost a lot of time, and the super call to neutron already run
    those, it can be removed.
    
    Change-Id: I729b3e18d52df4c458f7ea8d2180deb534a375f1

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -3609,9 +3609,6 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
     def get_port(self, context, id, fields=None):
         port = super(NsxV3Plugin, self).get_port(context, id, fields=None)
-        if 'id' in port:
-            port_model = self._get_port(context, port['id'])
-            resource_extend.apply_funcs('ports', port, port_model)
         self._extend_get_port_dict_qos_and_binding(context, port)
         self._remove_provider_security_groups_from_list(port)
         return db_utils.resource_fields(port, fields)
@@ -3628,16 +3625,6 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
                     limit, marker, page_reverse))
             # Add port extensions
             for port in ports[:]:
-                if 'id' in port:
-                    try:
-                        port_model = self._get_port(context, port['id'])
-                        resource_extend.apply_funcs('ports', port, port_model)
-                    except n_exc.PortNotFound:
-                        # Port might have been deleted by now
-                        LOG.debug("Port %s was deleted during the get_ports "
-                                  "process, and is being skipped", port['id'])
-                        ports.remove(port)
-                        continue
                 self._extend_get_port_dict_qos_and_binding(context, port)
                 self._remove_provider_security_groups_from_list(port)
         return (ports if not fields else
