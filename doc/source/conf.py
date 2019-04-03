8b48578f694936d31babc07fe34a77088e667ed6
Adit Sarfaty <asarfaty@vmware.com>
2019-04-03 10:40:54 +0300

    Retire oslosphinx
    Change-Id: I83f84d0b9feccbe042d9119564744e955bc0b54b

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'openstackdocstheme'
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
