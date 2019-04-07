6431cb9ffa2c7121d3235cc6b331612d49e54670
Adit Sarfaty <asarfaty@vmware.com>
2019-04-07 08:35:53 +0300

    Add DB migration milestones for Stein
    Change-Id: Ic7de8d60e7cb3736f61d40c8f7cd41eaeddaa6ff

--- vmware_nsx/db/migration/alembic_migrations/versions/queens/contract/717f7f63a219_nsxv3_lbaas_l7policy.py
+++ vmware_nsx/db/migration/alembic_migrations/versions/queens/contract/717f7f63a219_nsxv3_lbaas_l7policy.py
@@ -30,7 +30,7 @@ revision = '717f7f63a219'
 down_revision = 'a1be06050b41'
 
 # milestone identifier, used by neutron-db-manage
-neutron_milestone = [migration.QUEENS, migration.ROCKY]
+neutron_milestone = [migration.QUEENS, migration.ROCKY, migration.STEIN]
 
 
 def upgrade():
